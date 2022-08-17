36021d419358ca2eb29b515184684e8b8f1868c3
Jeremy Reyes B <jjrb6@hotmail.com>
2022-08-16 20:35:13 -0500

    error handler

---
+++ 23-error-handler/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.Create("names.txt")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer f.Close()
+
+	r := strings.NewReader("Jeremy Reyes B.")
+
+	io.Copy(f, r)
+
+	log.Println("log Println") // Log but not stop.
+	log.Fatalln("log Fatalln") // Log and stop with exit estatus.
+	panic("panic") // Stop completely and call the file called.
+}
